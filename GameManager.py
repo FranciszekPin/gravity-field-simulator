553756c09d271c28ad27a1b1e4c0398f5090e5f4
Franciszek Pindel <franciszek.pin@gmail.com>
2019-11-20 21:53:21 +0100

    Add SO MUCH fancy sky background

--- GameManager.py
+++ GameManager.py
@@ -5,8 +5,16 @@ class GameManager:
     def __init__(self):
         self.showBase = ShowBase()
         self.showBase.cam.setPos(0, -25, 0)
-        print("mess")
         ballsManager = BallsManager(self.showBase)
+        self.makeSky()
+
+    def makeSky(self):
+        self.sky = loader.loadModel("models/solar_sky_sphere")
+        self.sky_tex = loader.loadTexture("models/stars_1k_tex.jpg")
+        self.sky.setTexture(self.sky_tex, 1)
+        self.sky.reparentTo(render)
+        self.sky.setScale(40)
+
 
 # execute
 gameManager = GameManager()
