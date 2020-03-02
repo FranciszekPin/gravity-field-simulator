6a719b4171b6a3774106f28ed82569caf860a612
dowkie <widprzem@gmail.com>
2020-03-02 19:08:13 +0100

    smile_emoji

--- BallsManager.py
+++ BallsManager.py
@@ -9,31 +9,62 @@ class BallsManager:
     # every ball on the scene
     balls = []
 
+    speed = 100000
+
     def __init__(self, showBase):
         self.showBase = showBase
-        self.addBall(numpy.array([0., 0., 0.]), numpy.array([0., 0., 0.]))
-        self.addBall(numpy.array([0., 0., 5.]), numpy.array([.04, 0., 0.]))
+
+        self.addBall(numpy.array([0, 0., 0.]), numpy.array([0, 0.0, 0.0]), False, 333000, "sun")
+        self.addBall(numpy.array([0., 0., 100]), numpy.array([0.0004713, 0, 0]), False, 1, "earth")
+        self.addBall(numpy.array([0., 0., 50]), numpy.array([0.0006665, 0, 0]), False, 1, "mars")
+        self.addBall(numpy.array([0., 0., 25]), numpy.array([0.0009426, 0, 0]), False, 1, "mercury")
+
+        # these will orbit
+        #self.addBall(numpy.array([0., 0., 0.]), numpy.array([0, 0.0, 0.0]), static = True)
+        #self.addBall(numpy.array([-1.414213, -1.414213, 0]), numpy.array([0.0, 0, -0.18262]), static = False)
         self.showBase.taskMgr.add(self.updateBallsTask, "updateBallsTask")
         self.lastDeltaTime = 0
+        
+    def addBall(self, position, velocity, static, mass, texName):
+        # Using trial and error, I have successfully determined the radius of
+        # the sample model of ball and it happens to be 0.8 (what the hell)...
+        ballModel = self.showBase.loader.loadModel("models/ball")
+        tex = 0
+        ballModel.setScale(5)
 
-    def addBall(self, position, velocity):
-        self.ballModel = self.showBase.loader.loadModel("models/ball")
-        self.ballModel.reparentTo(self.showBase.render)
-        positionInVector3 = LVector3(position[0], position[1], position[2])
-        self.ballModel.setPos(positionInVector3)
-        self.balls.append(Ball(position, self.ballModel, False, velocity))
+        if texName == "sun":
+            tex = loader.loadTexture("models/sun_1k_tex.jpg")
+            ballModel.setScale(15)
+        elif texName == "earth":
+            tex = loader.loadTexture("models/earth_1k_tex.jpg")
+        elif texName == "mercury":
+            tex = loader.loadTexture("models/mercury_1k_tex.jpg")
+        elif texName == "mars":
+            tex = loader.loadTexture("models/mars_1k_tex.jpg")
+        elif texName == "moon":
+            tex = loader.loadTexture("models/moon_1k_tex.jpg")
+            ballModel.setScale(2)
+
+
+        ballModel.setTexture(tex)
+        # ...and therefore needs to be scaled so it's equal to 0.5
+        #ballModel.setScale(1.25, 1.25, 1.25)
+
+        ballModel.reparentTo(self.showBase.render)
+        ballModel.setPos(LVector3(position[0], position[1], position[2]))
+        self.balls.append(Ball(position, ballModel, static, velocity, mass))
 
     def updateBallsPositions(self, deltaTime):
         """ Moves all balls by their velocities """
         for ball in self.balls:
-            ball.move(LVector3(ball.velocity[0], ball.velocity[1], ball.velocity[2])*deltaTime)
+            ball.move(LVector3(ball.velocity[0], ball.velocity[1], ball.velocity[2]) * deltaTime)
 
     def updateBallsTask(self, task):
         """ Takes care of changing phsyics values of all objects """
 
-        deltaTime = (task.time-self.lastDeltaTime)*5
+        deltaTime = (task.time - self.lastDeltaTime) * self.speed
 
-        PhysicsManager().calculateForces(self.balls, deltaTime)
+        PhysicsManager().update(self.balls, deltaTime)
 
         # it should be called after all velocity calculations
         self.updateBallsPositions(deltaTime)
@@ -41,4 +72,3 @@ class BallsManager:
         self.lastDeltaTime = task.time
 
         return task.cont
-
--- GameManager.py
+++ GameManager.py
@@ -4,8 +4,11 @@ from BallsManager import BallsManager
 class GameManager:
     def __init__(self):
         self.showBase = ShowBase()
-        self.showBase.cam.setPos(0, -25, 0)
+        self.showBase.cam.setPos(0, -450, 0)
+
         ballsManager = BallsManager(self.showBase)
+
+        # lack of model
         self.makeSky()
 
     def makeSky(self):
@@ -13,7 +16,7 @@ class GameManager:
         self.sky_tex = loader.loadTexture("models/stars_1k_tex.jpg")
         self.sky.setTexture(self.sky_tex, 1)
         self.sky.reparentTo(render)
-        self.sky.setScale(40)
+        self.sky.setScale(400)
 
 
 # execute
