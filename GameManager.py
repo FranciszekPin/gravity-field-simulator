86a8f2dd2d082aee80af5cda917c0a207596ecb5
Franciszek Pindel <franciszek.pin@gmail.com>
2020-11-30 21:05:24 +0100

    Clear GameManager.py file

--- GameManager.py
+++ GameManager.py
@@ -5,17 +5,19 @@ from SkyManager import SkyManager
 class GameManager:
     def __init__(self):
         self.showBase = ShowBase()
-        self.showBase.cam.setPos(0, -450, 0)
-
+        
+        self.setCameraPosition(0, -450, 0)
         ballsManager = BallsManager(self.showBase)
 
-        # lack of model
         self.makeSky()
+
+    def setCameraPosition(self, x, y, z):
+        self.showBase.cam.setPos(0, -450, 0)
 
     def makeSky(self):
         self.sky = loader.loadModel("models/solar_sky_sphere")
-        self.sky_tex = loader.loadTexture("models/stars_1k_tex.jpg")
-        self.sky.setTexture(self.sky_tex, 1)
+        self.skyTexture = loader.loadTexture("models/stars_1k_tex.jpg")
+        self.sky.setTexture(self.skyTexture, 1)
         self.sky.reparentTo(render)
         self.sky.setScale(400)
 
