a93f2789b1e1ea7fd07ba47686e0d6dcae695be5
Franciszek Pindel <franciszek.pin@gmail.com>
2020-11-30 21:42:30 +0100

    Clear PhysicsManager.py file

--- physics/PhysicsManager.py
+++ physics/PhysicsManager.py
@@ -13,14 +13,9 @@ class PhysicsManager:
     def calculateForce(balls, deltaTime):
         """ Calculates gravitational force of two specific balls """
 
-        # vector from balls[1] to balls[0]
         vR = balls[0].position - balls[1].position
 
         sR = linalg.norm(vR)
-
-        # TODO --------
-        if sR == 0:
-            return
 
         # F = GMm/r^2
         F = (PhysicsManager.G * balls[0].mass * balls[1].mass) / (sR ** 2)
@@ -33,7 +28,7 @@ class PhysicsManager:
         balls[1].updateVelocity(F * deltaTime / balls[1].mass * vR)
 
     def update(self, balls, deltaTime):
-        """ Calculates forces & velocities after collision of every pair of balls possible """
+        """ Calculates forces of every pair of balls """
 
         # goes through each pair of balls, never twice and calculates forces
         i = 1
