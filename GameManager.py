1429c624cdf74910636f5dcbf128137df4ea255f
Franciszek Pindel <franciszek.pin@gmail.com>
2020-12-02 22:13:12 +0100

    Make GameManager.py compatible with PEP 8

--- GameManager.py
+++ GameManager.py
@@ -2,17 +2,19 @@ from direct.showbase.ShowBase import ShowBase
 from BallsManager import BallsManager
 from SkyManager import SkyManager
 
+
 class GameManager:
     def __init__(self):
         self.showBase = ShowBase()
         
-        self.setCameraPosition(0, -450, 0)
+        self.set_camera_position(0, -450, 0)
         BallsManager(self.showBase)
 
         SkyManager(self.showBase, self.showBase.cam)
 
-    def setCameraPosition(self, x, y, z):
+    def set_camera_position(self, x, y, z):
         self.showBase.cam.setPos(0, -450, 0)
+
 
 gameManager = GameManager()
 gameManager.showBase.run()
